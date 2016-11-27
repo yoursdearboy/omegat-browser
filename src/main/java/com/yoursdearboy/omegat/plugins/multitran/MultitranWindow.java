4e17288a91c0a4c1bd71450a85c534d8450cbfbf
Kirill Voronin <yoursdearboy@gmail.com>
2016-11-27 15:10:09 +0300

    Main class and MANIFEST

---
+++ src/main/java/com/yoursdearboy/omegat/plugins/multitran/MultitranWindow.java
@@ -0,0 +1,9 @@
+package com.yoursdearboy.omegat.plugins.multitran;
+
+/*
+ * @author Kirill Voronin (yoursdearboy@gmail.com)
+ */
+public class MultitranWindow {
+    public MultitranWindow() {
+    }
+}
