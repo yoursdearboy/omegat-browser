7a350f58a6a3a7b5d53fce670cabad9aa45dc28c
Kirill Voronin <kirill.voronin@fccho-moscow.ru>
2016-12-13 11:26:03 +0300

    New OmegaT plugins mechanism

---
+++ src/main/java/com/yoursdearboy/omegat/plugins/multitran/MultitranPlugin.java
@@ -0,0 +1,15 @@
+package com.yoursdearboy.omegat.plugins.multitran;
+
+/*
+ * @author Kirill Voronin (yoursdearboy@gmail.com)
+ */
+public class MultitranPlugin {
+    public MultitranPlugin() {
+    }
+
+    public static void loadPlugins() {
+    }
+
+    public static void unloadPlugins() {
+    }
+}
--- src/main/java/com/yoursdearboy/omegat/plugins/multitran/MultitranWindow.java
+++
@@ -1,9 +0,0 @@
-package com.yoursdearboy.omegat.plugins.multitran;
-
-/*
- * @author Kirill Voronin (yoursdearboy@gmail.com)
- */
-public class MultitranWindow {
-    public MultitranWindow() {
-    }
-}
