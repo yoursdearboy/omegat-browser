f58a0643f332ec6d5c13b45f26713a1be35511dc
Kirill Voronin <kirill.voronin@fccho-moscow.ru>
2016-12-23 17:40:18 +0300

    ScriptsPrefs containing last enabled scripts

--- src/main/java/com/yoursdearboy/omegat/plugins/browser/BrowserPlugin.java
+++ src/main/java/com/yoursdearboy/omegat/plugins/browser/BrowserPlugin.java
@@ -9,17 +9,18 @@ import org.omegat.util.StaticUtils;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.io.File;
-import java.io.FilenameFilter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 
 /*
  * @author Kirill Voronin (yoursdearboy@gmail.com)
  */
 public class BrowserPlugin {
     private static String SCRIPTS_DIR_NAME = "browser-scripts";
+    private static String SCRIPTS_PREFS_FILENAME = "browser-scripts.prefs";
     private static ScriptsRunner scriptsRunner;
     private static ScriptsMonitor scriptsMonitor;
+    private static ScriptsPrefs scriptsPrefs;
     private static FilenameFilter filenameFilter = new FilenameFilter() {
         @Override
         public boolean accept(File dir, String name) {
@@ -30,6 +31,7 @@ public class BrowserPlugin {
     public static void loadPlugins() {
         setupScriptsDir();
         setupScriptsRunner();
+        setupScriptsPrefs();
         CoreEvents.registerApplicationEventListener(new IApplicationEventListener() {
             @Override
             public void onApplicationStartup() {
@@ -48,25 +50,48 @@ public class BrowserPlugin {
     }
 
     private static void setupScriptsDir() {
-        File browserScriptsDir = getBrowserScriptsDir();
-        if (!browserScriptsDir.exists()) browserScriptsDir.mkdir();
+        File ScriptsDir = getScriptsDir();
+        if (!ScriptsDir.exists()) ScriptsDir.mkdir();
     }
 
     private static void setupScriptsRunner() {
         scriptsRunner = new ScriptsRunner();
-        scriptsMonitor = new ScriptsMonitor(scriptsRunner, getBrowserScriptsDir(), filenameFilter);
+        scriptsMonitor = new ScriptsMonitor(scriptsRunner, getScriptsDir(), filenameFilter);
     }
 
     private static void startScriptsRunner() {
-        for (File file : getBrowserScriptsDir().listFiles(filenameFilter)) {
+        java.util.List<File> scripts = scriptsPrefs.getEnabledScripts();
+        for (File file : getScriptsDir().listFiles(filenameFilter)) {
             scriptsRunner.add(file);
-            scriptsRunner.enable(file);
+            if (scripts.contains(file)) {
+                scriptsRunner.enable(file);
+            }
         }
         scriptsMonitor.start(true);
     }
 
-    private static File getBrowserScriptsDir() {
+    private static void setupScriptsPrefs() {
+        scriptsPrefs = new ScriptsPrefs(getScriptsPrefsFile(), getScriptsDir(), scriptsRunner);
+        scriptsPrefs.load();
+        CoreEvents.registerApplicationEventListener(new IApplicationEventListener() {
+            @Override
+            public void onApplicationStartup() {
+
+            }
+
+            @Override
+            public void onApplicationShutdown() {
+                scriptsPrefs.save();
+            }
+        });
+    }
+
+    private static File getScriptsDir() {
         return new File(StaticUtils.getConfigDir(), SCRIPTS_DIR_NAME);
+    }
+
+    private static File getScriptsPrefsFile() {
+        return new File(StaticUtils.getConfigDir(), SCRIPTS_PREFS_FILENAME);
     }
 
     private static void setupMenu() {
@@ -76,7 +101,7 @@ public class BrowserPlugin {
             @Override
             public void actionPerformed(ActionEvent event) {
                 try {
-                    Desktop.getDesktop().open(getBrowserScriptsDir());
+                    Desktop.getDesktop().open(getScriptsDir());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
---
+++ src/main/java/com/yoursdearboy/omegat/plugins/browser/ScriptsPrefs.java
@@ -0,0 +1,87 @@
+package com.yoursdearboy.omegat.plugins.browser;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
+class ScriptsPrefs implements ScriptsEventListener {
+    private final File file;
+    private final File scriptsDirectory;
+    private List<File> enabledScripts;
+
+    ScriptsPrefs(File file, File scriptsDirectory, ScriptsRunner scriptsRunner) {
+        this.file = file;
+        this.scriptsDirectory = scriptsDirectory;
+        scriptsRunner.registerEventListener(this);
+    }
+
+    void load() {
+        java.util.List<File> enabledScripts = new ArrayList<File>();
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = br.readLine()) != null) {
+                File script = new File(scriptsDirectory, line);
+                enabledScripts.add(script);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        this.enabledScripts = enabledScripts;
+    }
+
+    void save() {
+        StringBuilder out = new StringBuilder();
+        for (File script : enabledScripts) {
+            out.append(script.getName());
+            if (enabledScripts.indexOf(script) < enabledScripts.size()-1) out.append("\n");
+        }
+        BufferedWriter br = null;
+        try {
+            br = new BufferedWriter(new FileWriter(file));
+            br.write(out.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    List<File> getEnabledScripts() {
+        return enabledScripts;
+    }
+
+    @Override
+    public void onAdd(File file) {
+    }
+
+    @Override
+    public void onEnable(File file) {
+        if (!enabledScripts.contains(file)) {
+            enabledScripts.add(file);
+        }
+    }
+
+    @Override
+    public void onDisable(File file) {
+        enabledScripts.remove(file);
+    }
+
+    @Override
+    public void onRemove(File file) {
+    }
+}
