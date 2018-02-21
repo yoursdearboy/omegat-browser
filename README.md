7bdf902b9552a8db0bdf0553bfaeb1a1a2376459
Kirill Voronin <yoursdearboy@gmail.com>
2018-02-21 16:57:50 +0300

    Script for Google

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ Download using Right click -> Save as. Name must end with `.groovy`
 * [Multitran](https://raw.githubusercontent.com/yoursdearboy/omegat-browser/v1.1/scripts/multitran.groovy) - lookup selected or focused word in [multitran.ru](multitran.ru) dictionary. Shortcut: `CTRL + ALT + M`.
 * [Lingvo Live](https://raw.githubusercontent.com/yoursdearboy/omegat-browser/v1.1/scripts/lingvolive.groovy) - lookup selected or focused word in [lingvolive.ru](lingvolive.ru) dictionary. Shortcut: `CTRL + ALT + L`. If **all** symbols are broken - try to install [Roboto fonts](https://storage.googleapis.com/material-design/publish/material_v_10/assets/0B0J8hsRkk91LRjU4U1NSeXdjd1U/RobotoTTF.zip).
 * [Deepl](https://raw.githubusercontent.com/yoursdearboy/omegat-browser/v1.1/scripts/lingvolive.groovy) - translates current entry using [Deepl](https://deepl.com). Choose language manually. Contributed by [@rosros2000](https://github.com/rosros2000).
+* [Google Search](https://raw.githubusercontent.com/yoursdearboy/omegat-browser/v1.1/scripts/google.groovy) - search current entry using [Google](https://google.com).
 
 *On Mac use `CMD` instead of `Ctrl` for shortcuts.*
 
---
+++ scripts/google.groovy
@@ -0,0 +1,86 @@
+import com.sun.glass.events.KeyEvent
+import org.omegat.core.Core
+import org.omegat.core.CoreEvents
+import org.omegat.core.events.IEditorEventListener
+import org.omegat.gui.editor.IPopupMenuConstructor
+import org.omegat.gui.editor.SegmentBuilder
+import org.omegat.gui.main.MainWindow
+
+import javax.swing.AbstractAction
+import javax.swing.Action
+import javax.swing.JComponent
+import javax.swing.JMenuItem
+import javax.swing.JPopupMenu
+import javax.swing.KeyStroke
+import javax.swing.text.JTextComponent
+import java.awt.event.ActionEvent
+
+def FILENAME = "google.groovy"
+def KEY = "GOOGLE"
+def TITLE = "Google"
+def DOMAIN = /^google/
+def URL = "google.com/search"
+
+def pane = BrowserPane.get(KEY, TITLE, DOMAIN)
+pane.getBrowser().loadURL(URL)
+
+/* Record word at caret */
+String caretWord = null
+def editorEventListener = new IEditorEventListener() {
+    @Override
+    void onNewWord(String s) {
+        caretWord = s
+    }
+}
+CoreEvents.registerEditorEventListener(editorEventListener)
+
+/* Main action that opens Google */
+Action action = new AbstractAction() {
+    @Override
+    void actionPerformed(ActionEvent e) {
+        q = ScriptHelpers.prepareText(Core.getEditor().getSelectedText(), caretWord)
+        if (q == null) return
+        url = "http://${URL}?"
+        url += "q=${ScriptHelpers.encodeText(q)}"
+        pane.getBrowser().loadURL(url)
+    }
+}
+
+// FIXME: Remove menu items
+/* Popup menu item */
+Core.getEditor().registerPopupMenuConstructors(1000, new IPopupMenuConstructor() {
+    @Override
+    void addItems(JPopupMenu menu, JTextComponent comp, int mousepos, boolean isInActiveEntry,
+                  boolean isInActiveTranslation, final SegmentBuilder sb) {
+        if (isInActiveEntry) {
+            menu.addSeparator()
+            JMenuItem menuItem = menu.add("Search in Google")
+            menuItem.addActionListener(action)
+        }
+    }
+})
+
+/* Bind hotkey: Ctrl + ALT + G */
+MainWindow mainWindow = (MainWindow) Core.getMainWindow()
+int COMMAND_MASK = System.getProperty("os.name").contains("OS X") ? ActionEvent.META_MASK : ActionEvent.CTRL_MASK
+KeyStroke keystroke = KeyStroke.getKeyStroke(KeyEvent.VK_G, ActionEvent.ALT_MASK + COMMAND_MASK)
+def actionMapKey = "searchInGoogle"
+mainWindow.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(keystroke, actionMapKey)
+mainWindow.getRootPane().getActionMap().put(actionMapKey, action)
+
+/* Remove all this when script is disabled */
+def scriptsEventListener = [
+        onAdd    : {},
+        onEnable : {},
+        onRemove : {},
+        onDisable: { File file ->
+            if (file.getName() == FILENAME) {
+                pane.close()
+                CoreEvents.unregisterEditorEventListener(editorEventListener)
+                mainWindow.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).remove(keystroke)
+                mainWindow.getRootPane().getActionMap().remove(actionMapKey)
+                scriptsRunner.unregisterEventListener(scriptsEventListener)
+            }
+        }].asType(ScriptsEventListener)
+
+scriptsRunner.registerEventListener(scriptsEventListener)
